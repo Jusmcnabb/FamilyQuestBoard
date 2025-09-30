22fc48d2064e7b1519c2c4edc4cd174212b107b0
jusmcnabb <jusmcnabb@gmail.com>
2025-09-30 15:06:03 -0500

    Add project files.

---
+++ FamilyQuestBoard/Models/Quest.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FamilyQuestBoard.Models
+{
+    public class Quest
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime CompletionDate { get; set; }
+        public int Points { get; set; }
+
+    }
+}
